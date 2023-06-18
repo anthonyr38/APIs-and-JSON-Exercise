c7837d9465afbd190c3da03e96b040cee0b7e83a
Anthony Rodriguez <anthonyr38@proton.me>
2023-06-17 22:12:58 -0400

    api json exercise 1

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        public static void Conversation()
+        {
+            var client = new HttpClient();
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine($"Ron:\n{SwansonQuote(client)}\n");
+                Thread.Sleep(3000);
+                Console.WriteLine($"Kanye:\n{KanyeQuote(client)}\n");
+                Thread.Sleep(3000);
+            }
+        }
+        private static string SwansonQuote(HttpClient client)
+        {
+            var jText = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+
+            var quote = JArray.Parse(jText).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+            return quote;
+        }
+
+        private static string KanyeQuote(HttpClient client)
+        {
+            var jText = client.GetStringAsync("https://api.kanye.rest/").Result;
+
+            var quote = JObject.Parse(jText).GetValue("quote").ToString();
+
+            return quote;
+        }
     }
 }
